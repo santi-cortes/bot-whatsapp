360c6be8ae3567ec269de75f39e0165acd452674
santya.2003.06@gmail.com <santiagocorts@blife.mx>
2023-12-13 21:47:59 -0600

    fix not box problem in deploy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Client } = require("whatsapp-web.js");
 const express = require("express");
 const qrcode = require("qrcode");
 const http = require("http");
+const puppeteer = require("puppeteer");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,6 +11,7 @@ const io = require("socket.io")(server);
 app.use(express.static("public"));
 
 const client = new Client();
+let browser;
 
 client.on("qr", (qr) => {
   qrcode.toDataURL(qr, (err, url) => {
@@ -25,8 +27,9 @@ client.on("auth_failure", (msg) => {
   console.error("Error de autenticación:", msg);
 });
 
-client.on("ready", () => {
+client.on("ready", async () => {
   console.log("Cliente listo");
+  browser = await puppeteer.launch({ args: ["--no-sandbox"] });
 });
 
 client.on("message", (message) => {
@@ -37,4 +40,10 @@ client.initialize();
 
 server.listen(3000, () => {
   console.log("Servidor en http://localhost:3000");
+});
+
+// Manejar el cierre adecuado
+process.on("SIGINT", async () => {
+  await browser.close();
+  process.exit();
 });
